484091d466bd3303600d24c5e1af3b2966733087
root <root@ip-172-31-94-165.ec2.internal>
2024-02-15 03:16:13 +0000

    myterra file

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-   region = us-east-1
-   access_key = "AKIAWHD3AQVIXEODXBUH"
-   secret_key = "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"
+provider "aws"{
+  region = "us-east-1"
+  access_key = "AKIAWHD3AQVIXEODXBUH"
+  secret_key = "t0K1k5Tx0E+RhkCqmFDXjgaFAHShr1iJslzWocDY"
 }
 
-
-resource "aws_s3_bucket" "example" {
-    bucket = "faruk231"
+resource "aws_s3_bucket" {
+     bucket = "faruk6283"
 }
+
